7544564bf990c1983f910132b4723a938d22c8ed
oweslley <wesleymoraeswv03@hotmail.com>
2025-01-14 21:56:49 -0300

    remove-duplicates-from-sorted-array

---
+++ remove-duplicates-from-sorted-array.js
@@ -0,0 +1,38 @@
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var removeDuplicates = function(nums) {
+//   let set = new Set(nums); 
+//   const arr = [...set];
+//   const x = nums.length - arr.length
+//   const k = arr.length
+//   let i = 0;
+//   do {
+//     arr.push('_')
+//     i++
+//   } while (i < x);  
+//   return arr
+// };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  let index = 1
+  for(let i = 0; i < nums.length -1; i++) {
+    if(nums[i] !== nums[i+1]) {
+      nums[index] = nums[i+1];
+      index++
+    }
+  }
+  return index; 
+};
+
+console.log(removeDuplicates([1,1,2]));
+
+// const case01 = [1,1,2] //[1,2,_]
+// const case02 = [0,0,1,1,1,2,2,3,3,4] //[0,1,2,3,4,_,_,_,_,_]
+// console.log(removeDuplicates(case01));
+// console.log(removeDuplicates(case02));
