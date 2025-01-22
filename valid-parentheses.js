36fa744ecc384aadc09d742af676faf5168e43ae
oweslley03 <wesleymoraeswv03@hotmail.com>
2025-01-21 22:47:59 -0300

    refact valid-parentheses.js

--- valid-parentheses.js
+++ valid-parentheses.js
@@ -3,24 +3,33 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-  const stack = [];
-  const parents = `() [] {}`;
-  let i = 0;
-
-  while(i < s.length) {
-    stack.push(s[i]);
-    i++;
-
-    let open = stack[stack.length -2]
-    let closed = stack[stack.length -1]
-
-    let pair = open + closed;
-    if(parents.includes(pair)) {
+  const couples = [ '()', '[]', '{}' ]
+  let stack = [];
+  for(let i = 0; i < s.length; i++) {
+    stack.push(s[i])
+    let pair = stack[stack.length - 2] + stack[stack.length - 1]
+    if(couples.includes(pair)) {
       stack.pop()
       stack.pop()
     }
-  };
-  return stack.length === 0;
+  }
+  return stack.length === 0
 };
 
-console.log(isValid('({(}})[}}{}}'));
+/*
+give an string 's' containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+no caso, se todos são um casal. 
+
+'ab', 'cd', 'ef'
+abcdefgi
+(a, b) existe nos casais?
+cdefgi
+(c, d) existe nos casais?
+efgi
+(e, f) existe nos casais?
+*/   
+
+console.log(isValid("()")); //true
+console.log(isValid("()[]{}")); //true
+console.log(isValid("(]")); //false
+console.log(isValid("([])")); //true
