1f8ec564d2d7e526f02273722673d9ce1ddc24a4
oweslley03 <wesleymoraeswv03@hotmail.com>
2025-02-02 18:49:12 -0300

    fix find-the-index-of-the-first-occurrence-in-a-string.js

--- find-the-index-of-the-first-occurrence-in-a-string.js
+++ find-the-index-of-the-first-occurrence-in-a-string.js
@@ -7,8 +7,11 @@ var strStr = function(haystack, needle) {
   for(let i=0; i < haystack.length;i++) {
     if(haystack[i] === needle[0]) {
       const sub = haystack.substring(i, i+needle.length)
-      if(sub === needle) return haystack[i] 
+      if(sub === needle) return i
     }
   }
   return -1;
 };
+
+console.log(strStr("sadbutsad", "sad"))
+console.log(strStr("leetcode", "leeto"))
